25c021fa908aaa19170e67a23d8888105fe2010b
Alperen Mutlu <chetto_17@hotmail.com>
2021-05-26 23:38:52 +0300

    Bugs fixed.

--- hrms/src/main/java/project/hrms/business/concretes/AuthManager.java
+++ hrms/src/main/java/project/hrms/business/concretes/AuthManager.java
@@ -20,7 +20,7 @@ import project.hrms.entities.concretes.JobSeeker;
 import project.hrms.entities.concretes.VerificationCode;
 
 @Service
-public class AuthManager implements AuthService{
+public class AuthManager implements AuthService {
 
 	private UserService userService;
 	private EmployerService employerService;
@@ -28,6 +28,7 @@ public class AuthManager implements AuthService{
 	private VerificationService verificationService;
 	private ValidationService validationService;
 	private VerificationCodeService verificationCodeService;
+
 	
 	@Autowired
 	public AuthManager(UserService userService, EmployerService employerService, JobSeekerService jobSeekerService,
@@ -41,11 +42,10 @@ public class AuthManager implements AuthService{
 		this.validationService = validationService;
 		this.verificationCodeService = verificationCodeService;
 	}
-	
-	
+
 	@Override
 	public Result registerEmployer(Employer employer, String confirmPassword) {
-		
+
 		if (!checkIfNullInfoForEmployer(employer)) {
 
 			return new ErrorResult("You have entered missing information. Please fill in all fields.");
@@ -70,17 +70,18 @@ public class AuthManager implements AuthService{
 		String code = verificationService.sendCode();
 		verificationCodeRecord(code, employer.getId(), employer.getEmail());
 		return new SuccessResult("Registration has been successfully completed");
+
 	}
 
 	@Override
 	public Result registerJobSeeker(JobSeeker jobSeeker, String confirmPassword) {
-		
+
 		if (checkIfRealPerson(Long.parseLong(jobSeeker.getNationalId()), jobSeeker.getFirstName(),
 				jobSeeker.getLastName(), jobSeeker.getDateOfBirth().getYear()) == false) {
 			return new ErrorResult("TCKN could not be verified.");
 		}
 
-		if (!checkIfNullInfoForJobseeker(jobSeeker, confirmPassword)) {
+		if (!checkIfNullInfoForJobSeeker(jobSeeker, confirmPassword)) {
 
 			return new ErrorResult("You have entered missing information. Please fill in all fields.");
 		}
@@ -101,97 +102,94 @@ public class AuthManager implements AuthService{
 		verificationCodeRecord(code, jobSeeker.getId(), jobSeeker.getEmail());
 		return new SuccessResult("Registration has been successfully completed");
 	}
-	
+
 	// Validation for employer register ---START---
 
-		private boolean checkIfNullInfoForEmployer(Employer employer) {
+	private boolean checkIfNullInfoForEmployer(Employer employer) {
 
-			if (employer.getCompanyName() != null && employer.getWebsite() != null && employer.getEmail() != null
-					&& employer.getPhoneNumber() != null && employer.getPassword() != null) {
+		if (employer.getCompanyName() != null && employer.getWebsite() != null && employer.getEmail() != null
+				&& employer.getPhoneNumber() != null && employer.getPassword() != null) {
 
-				return true;
+			return true;
 
-			}
-
-			return false;
 		}
 
-		private boolean checkIfEqualEmailAndDomain(String email, String website) {
-			String[] emailArr = email.split("@", 2);
-			String domain = website.substring(4, website.length());
+		return false;
+	}
 
-			if (emailArr[1].equals(domain)) {
+	private boolean checkIfEqualEmailAndDomain(String email, String website) {
+		String[] emailArr = email.split("@", 2);
+		String domain = website.substring(4, website.length());
 
-				return true;
-			}
-
-			return false;
-		}
-
-		// Validation for employer register ---END---
-
-		
-		// Validation for jobSeeker register ---START---
-		
-		private boolean checkIfNullInfoForJobseeker(JobSeeker jobSeeker, String confirmPassword) {
-
-			if (jobSeeker.getFirstName() != null && jobSeeker.getLastName() != null && jobSeeker.getNationalId() != null
-					&& jobSeeker.getDateOfBirth() != null && jobSeeker.getPassword() != null && jobSeeker.getEmail() != null
-					&& confirmPassword != null) {
-
-				return true;
-
-			}
-
-			return false;
-		}
-
-		private boolean checkIfExistsTcNo(String nationalId) {
-
-			if (this.jobSeekerService.getJobSeekerByNationalId(nationalId).getData() == null) {
-				return true;
-			}
-			return false;
-		}
-
-		private boolean checkIfRealPerson(long nationalId, String firstName, String lastName, int yearOfBirth) {
-
-			if (validationService.validateByMernis(nationalId, firstName, lastName, yearOfBirth)) {
-				return true;
-			}
-			return false;
-		}
-
-		// Validation for jobSeeker register ---END---
-
-
-		// Common Validation
-
-		private boolean checkIfEmailExists(String email) {
-
-			if (this.userService.getUserByEmail(email).getData() == null) {
-
-				return true;
-			}
-
-			return false;
-		}
-
-		private boolean checkIfEqualPasswordAndConfirmPassword(String password, String confirmPassword) {
-
-			if (!password.equals(confirmPassword)) {
-				return false;
-			}
+		if (emailArr[1].equals(domain)) {
 
 			return true;
 		}
-		
-		private void verificationCodeRecord(String code, int id, String email) {
-			
-			VerificationCode verificationCode = new VerificationCode(id, code, false, LocalDate.now());
-			this.verificationCodeService.add(verificationCode);
-			System.out.println("Verification code has been sent to " + email );
-		
+
+		return false;
+	}
+
+	// Validation for employer register ---END---
+
+	// Validation for jobSeeker register ---START---
+	
+	private boolean checkIfNullInfoForJobSeeker(JobSeeker jobSeeker, String confirmPassword) {
+
+		if (jobSeeker.getFirstName() != null && jobSeeker.getLastName() != null && jobSeeker.getNationalId() != null
+				&& jobSeeker.getDateOfBirth() != null && jobSeeker.getPassword() != null && jobSeeker.getEmail() != null
+				&& confirmPassword != null) {
+
+			return true;
+
 		}
+
+		return false;
+	}
+
+	private boolean checkIfExistsTcNo(String nationalId) {
+
+		if (this.jobSeekerService.getJobSeekerByNationalId(nationalId).getData() == null) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkIfRealPerson(long nationalId, String firstName, String lastName, int yearOfBirth) {
+
+		if (validationService.validateByMernis(nationalId, firstName, lastName, yearOfBirth)) {
+			return true;
+		}
+		return false;
+	}
+
+	// Validation for jobSeeker register ---END---
+
+	// Common Validation
+
+	private boolean checkIfEmailExists(String email) {
+
+		if (this.userService.getUserByEmail(email).getData() == null) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean checkIfEqualPasswordAndConfirmPassword(String password, String confirmPassword) {
+
+		if (!password.equals(confirmPassword)) {
+			return false;
+		}
+
+		return true;
+	}
+	
+	private void verificationCodeRecord(String code, int id, String email) {
 		
+		VerificationCode verificationCode = new VerificationCode(id, code, false, LocalDate.now());
+		this.verificationCodeService.add(verificationCode);
+		System.out.println("Verification code has been sent to " + email );
+	
+	}
 }
--- hrms/src/main/java/project/hrms/core/utilities/adapters/MernisServiceAdapter.java
+++ hrms/src/main/java/project/hrms/core/utilities/adapters/MernisServiceAdapter.java
@@ -1,22 +1,29 @@
 package project.hrms.core.utilities.adapters;
 
+import org.springframework.stereotype.Service;
+
 import project.hrms.mernisService.FakeMernisService;
 
-public class MernisServiceAdapter  implements ValidationService{
+@Service
+public class MernisServiceAdapter implements ValidationService {
 
-	
 	@Override
 	public boolean validateByMernis(long nationalId, String firstName, String lastName, int yearOfBirth) {
+
 		FakeMernisService client = new FakeMernisService();
 		
 		boolean result = true;
 		try {
 			result = client.ValidateByPersonalInfo(nationalId, firstName, lastName, yearOfBirth);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		return result;
+		
 	}
 
+	
+
 }
+
+
