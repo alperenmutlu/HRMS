274db6ec15d11bd10a19d7623e00202f6ecef537
Alperen Mutlu <chetto_17@hotmail.com>
2021-05-30 14:09:58 +0300

    JobAdvert işlemleri yapıldı.

---
+++ hrms/src/main/java/project/hrms/business/abstracts/JobAdvertService.java
@@ -0,0 +1,20 @@
+package project.hrms.business.abstracts;
+
+import java.util.List;
+
+import project.hrms.core.utilities.results.DataResult;
+import project.hrms.core.utilities.results.Result;
+import project.hrms.entities.concretes.JobAdvert;
+
+public interface JobAdvertService {
+	Result add(JobAdvert jobAdvert);
+	Result update(JobAdvert jobAdvert);
+	Result delete(int id);
+	Result changeOpenToClose(int id);
+	
+	DataResult<JobAdvert> getById(int id);	
+	DataResult<List<JobAdvert>> getAll();
+	DataResult<List<JobAdvert>> getAllOpenJobAdvertList();
+	DataResult<List<JobAdvert>> findAllByOrderByPublishedAt();
+	DataResult<List<JobAdvert>> getAllOpenJobAdvertByEmployer(int id);
+}
