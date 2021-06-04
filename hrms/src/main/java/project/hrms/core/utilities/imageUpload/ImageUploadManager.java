8c2f81be68180b1b2a3abebc9a8474661bb62d97
Alperen Mutlu <chetto_17@hotmail.com>
2021-06-04 23:12:54 +0300

    Cloudinary imageUpload için gerekli işlem yapıldı.

---
+++ hrms/src/main/java/project/hrms/core/utilities/imageUpload/ImageUploadManager.java
@@ -0,0 +1,43 @@
+package project.hrms.core.utilities.imageUpload;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import project.hrms.core.utilities.results.DataResult;
+import project.hrms.core.utilities.results.ErrorDataResult;
+import project.hrms.core.utilities.results.SuccessDataResult;
+
+@Service
+public class ImageUploadManager implements ImageUploadService{
+
+	private Cloudinary cloudinary;
+	
+	public ImageUploadManager() {
+
+		this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+				"cloud_name", "dsocr1qmt",
+				"api_key", "342388848912649",
+				"api_secret", "tM8kJb2qj5lZs5FB0NY26K5wo04"));
+	}
+
+	@Override
+	public DataResult<Map> uploadImageFile(MultipartFile imageFile) {
+		try {
+			@SuppressWarnings("unchecked")
+			Map<String, String> resultMap =(Map<String, String>) cloudinary.uploader().upload(imageFile.getBytes(), ObjectUtils.emptyMap());
+			return new SuccessDataResult<Map>(resultMap);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+		return new ErrorDataResult<Map>();
+	}
+
+}
