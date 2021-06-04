8cf622b0d82908b5076a27619bbf4fa32f455165
Alperen Mutlu <chetto_17@hotmail.com>
2021-06-05 00:29:53 +0300

    getJobSeekerCVById metodu eklendi.

--- hrms/src/main/java/project/hrms/api/controllers/JobSeekersController.java
+++ hrms/src/main/java/project/hrms/api/controllers/JobSeekersController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import project.hrms.business.abstracts.JobSeekerService;
 import project.hrms.core.utilities.results.DataResult;
 import project.hrms.core.utilities.results.Result;
 import project.hrms.entities.concretes.JobSeeker;
+import project.hrms.entities.dtos.JobSeekerCVDto;
 
 
 @RestController
@@ -28,28 +30,33 @@ public class JobSeekersController {
 	}
 		
 	@PostMapping("/add")
-	public Result add(@RequestBody JobSeeker jobSeeker){
-		return this.jobSeekerService.add(jobSeeker);
+	public Result add(@RequestBody JobSeeker jobseeker){
+		return this.jobSeekerService.add(jobseeker);
 	}
 //	
 //	@PostMapping("/update")
-//	public Result update(@RequestBody Jobseeker jobseeker){
-//		return this.jobseekerService.update(jobseeker);
+//	public Result update(@RequestBody JobSeeker jobSeeker){
+//		return this.jobSeekerService.update(jobSeeker);
 //	}
 //	
 //	@PostMapping("/delete")
-//	public Result delete(@PathVariable("id") int id){
-//		return this.jobseekerService.delete(id);
+//	public Result delete(@RequestParam("id") int id){
+//		return this.jobSeekerService.delete(id);
 //	}
 //	
 //	@GetMapping("/getbyid")
-//	public DataResult<Jobseeker> getById(@PathVariable("id") int id){
-//		return this.jobseekerService.getById(id);
+//	public DataResult<JobSeeker> getById(@RequestParam("id") int id){
+//		return this.jobSeekerService.getById(id);
 //	}
 	
 	@GetMapping("/getall")
 	public DataResult<List<JobSeeker>> getAll(){
 		return this.jobSeekerService.getAll();
+	}
+	
+	@GetMapping("/getJobSeekerCVById")
+	public DataResult<JobSeekerCVDto> getJobseekerCVById(@RequestParam int id){
+		return this.jobSeekerService.getJobSeekerCVById(id);
 	}
 	
 }
